41785459da33a13d03ed58a19b6a7ec7982c033f
Lucas Leitao <lucassilvaleitao1401@gmail.com>
2024-03-23 12:36:24 -0300

    atualizacao

--- logica.js
+++ logica.js
@@ -72,7 +72,49 @@ let cel = 15
 let far = cel * 1.8 + 32
 console.log(`Temperatura ${cel}° Celsius corresponde à ${far.toFixed(1)}° Farenheit`)
 
-    
+// //11. Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
+// a. o produto do dobro do primeiro com metade do segundo .
+// b. a soma do triplo do primeiro com o terceiro.
+// c. o terceiro elevado ao cubo.
+
+let n1 = 3 
+let n2 = 5 //int
+let n3 = 4.2 //ponto flutuante
+
+console.log((n1*2)*(n2/2))
+console.log((n1*3) + n3)
+console.log((n3**3).toFixed(3))
+
+// 12. Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58
+let altura = 1.71
+let form = 72.7 * altura - 58
+
+console.log(`O peso ideal para a sua altura, corresponde a: ${form.toFixed(2)}Kg`)
+
+// 13. Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu peso ideal, utilizando as seguintes fórmulas:
+// . Para homens: (72.7*h) - 58
+// a. Para mulheres: (62.1*h) - 44.7
+let ah = 1.71
+let am = 1.49
+let formh = 72.7 * ah - 58
+let formm = 62.1 * am - 44.7
+
+console.log(`O peso ideal para a altura ${ah}m corresponde a: ${formh.toFixed(2)}Kg para homens`)
+console.log(`O peso ideal para a altura ${am}m corresponde a: ${formm.toFixed(2)}Kg para mulheres`)
+
+// 14. João Papo-de-Pescador, homem de bem, comprou um microcomputador para controlar o rendimento diário de seu trabalho. Toda vez que ele traz um peso de peixes maior que o estabelecido pelo regulamento de pesca do estado de São Paulo (50 quilos) deve pagar uma multa de R$ 4,00 por quilo excedente. João precisa que você faça um programa que leia a variável peso (peso de peixes) e calcule o excesso. Gravar na variável excesso a quantidade de quilos além do limite e na variável multa o valor da multa que João deverá pagar. Imprima os dados do programa com as mensagens adequadas.
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
